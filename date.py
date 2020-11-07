85b54cb9b744277586218b125460abffa4466d60
MaksimKatorgin <72994036+MaksimKatorgin@users.noreply.github.com>
2020-11-07 11:46:37 +0300

    Add files via upload

---
+++ date.py
@@ -0,0 +1,6 @@
+import time
+from datetime import datetime, date
+date_time = datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+m= datetime.now().strftime("%m")
+print(m)
+print("Дата и время:", date_time)
