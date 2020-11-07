e0344e48e5a25bc8c178b5a6656fb68ad939c500
MaksimKatorgin <72994036+MaksimKatorgin@users.noreply.github.com>
2020-11-07 14:12:26 +0300

    Update date.py

--- date.py
+++ date.py
@@ -2,5 +2,8 @@ import time
 from datetime import datetime, date
 date = datetime.now().strftime("%d.%m.%Y")
 time = datetime.now().strftime("%H:%M:%S")
-print("Дата:", date)
-print("Время:", time)
+
+if date>=23 and month>=9:
+    print(date, "leto")
+elif date>=21 and month>=3:
+    print(date, "zima")
