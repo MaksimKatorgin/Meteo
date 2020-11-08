232db736c566ba1ff01156810f1abe7e92e85cfa
MaksimKatorgin <72994036+MaksimKatorgin@users.noreply.github.com>
2020-11-08 12:29:51 +0300

    Delete date.py

--- date.py
+++
@@ -1,9 +0,0 @@
-import time
-from datetime import datetime, date
-date = datetime.now().strftime("%d.%m.%Y")
-time = datetime.now().strftime("%H:%M:%S")
-
-if date>=23 and month>=9:
-    print(date, "leto")
-elif date>=21 and month>=3:
-    print(date, "zima")
