940e1e848deadd5e3621194ebeb3e10dfb90b923
MaksimKatorgin <72994036+MaksimKatorgin@users.noreply.github.com>
2020-11-08 14:24:23 +0300

    Update date.py

--- date.py
+++ date.py
@@ -3,7 +3,7 @@ date = datetime.now().strftime("%d.%m.%Y")
 time = datetime.now().strftime("%H:%M:%S")
 day = int(datetime.now().strftime("%d"))
 month = int(datetime.now().strftime("%m"))
-if day>=23 and month>=9:
-    print(day, month, "zima")
-elif day>=21 and month>=3:
+if day>=21 and month>=3:
     print(day, month, "leto")
+elif day>=23 and month>=9:
+    print(day, month, "zima")
