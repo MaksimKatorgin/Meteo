4ff093d766798448346bbf16196c50243323f934
MaksimKatorgin <72994036+MaksimKatorgin@users.noreply.github.com>
2020-11-08 13:41:24 +0300

    Add files via upload

---
+++ date.py
@@ -0,0 +1,9 @@
+from datetime import datetime, date
+date = datetime.now().strftime("%d.%m.%Y")
+time = datetime.now().strftime("%H:%M:%S")
+day = int(datetime.now().strftime("%d"))
+month = int(datetime.now().strftime("%m"))
+if day>=23 and month>=9:
+    print(day, month, "leto")
+elif day>=21 and month>=3:
+    print(day, month, "zima")
